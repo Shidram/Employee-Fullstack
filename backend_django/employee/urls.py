f65e0c7a601c13fefa4bf36748225fc03794e2c8
Shidramshetti Pasarad <shidramshetty35@gmail.com>
2021-09-13 12:28:15 +0530

    added EmployeeSearchAPIView

--- backend_django/employee/urls.py
+++ backend_django/employee/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from employee.views import (
     EmployeeAPIView,
     EmployeeUpdateAPIView,
-    EmployeeDeleteAPIView)
+    EmployeeDeleteAPIView,
+    EmployeeSearchAPIView)
 
 urlpatterns = [
     path('', EmployeeAPIView.as_view()),
     path('update/<int:pk>', EmployeeUpdateAPIView.as_view()),
     path('delete/<int:pk>', EmployeeDeleteAPIView.as_view()),
+    path('search/<str:firstname>', EmployeeSearchAPIView.as_view()),
 ]
